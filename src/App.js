84b5689daa3cb95795054ddf9cd63ca23399e73f
zengxianguo <584953740@qq.com>
2021-03-06 10:34:16 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App;
