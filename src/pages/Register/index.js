0deb0be403f78fa6e378600d7ee6d2d2b47e10a5
zengxianguo <54348897+zengxianguo@users.noreply.github.com>
2021-03-06 15:09:35 +0800

    添加注册页面

---
+++ src/pages/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <h1>你好，这里是注册页面</h1>
+      <a href="tel:10086">拨打电话给10086小姐姐</a>
+      <a href="mailto:young.joway@aliyun.com">发送邮件给JowayYoung</a>
+    </div>
+  )
+}
+
+export default Register
