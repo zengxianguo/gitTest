6ee6ffcebe5c107d935e2ffc31dd963204677588
zengxianguo <54348897+zengxianguo@users.noreply.github.com>
2021-03-06 15:34:24 +0800

    修改App.js页面

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from 'react'
+// import React from 'react'
+// import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+// function App() {
+//   return (
+//     <Router>
+//       <Switch>
+//         <Route></Route>
+//       </Switch>
+//     </Router>
+//   )
+// }
+// export default App;
 
+
+import React from 'react'
+import { Provider } from 'react-redux'
 import { HashRouter as Router, Switch, Route } from 'react-router-dom'
-function App() {
+import store from './store'
+
+
+import Longin from './pages/Login'
+import Register from './pages/Register'
+import BaseLayout from './layouts/BaseLayout'
+import PrivateRoute from './utils/PrivateRoute'
+
+const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route></Route>
-      </Switch>
-    </Router>
+    <Provider store={ store }>
+      <Router>
+        <Switch>
+          <Route path="/login" component={ Longin }></Route>
+          <Route path="/register" component={ Register }></Route>
+          <PrivateRoute path="/" component={ BaseLayout }></PrivateRoute>
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
 
-export default App;
+export default App
